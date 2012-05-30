08948be3a03b092f728117d9d9d8f346d962a2fc
Don Nolder <dnolder@gmail.com>
2012-05-30 15:44:23 +1000

    Update 4

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (testjsx) is running on Node.JS ' + process.version);
+  res.end('This is it!\nApp (testjsx) is running on Node.JS ' + process.version);
 }).listen(process.env['app_port'] || 3000);
